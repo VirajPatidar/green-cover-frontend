d6b4cddd318b881bce99ceffec588eeb7e0982d0
Viraj Patidar <vrj381@gmail.com>
2021-12-11 17:21:57 +0530

    basic map

---
+++ src/components/test.js
@@ -0,0 +1,17 @@
+const Test = () => {
+    return (
+        <>
+            <h1>It Works</h1>
+            <br />
+            <iframe id="inlineMapFrame"
+                title="map"
+                width="700"
+                height="500"
+                src="http://localhost:8000/">
+            </iframe>
+        </>
+
+    );
+}
+ 
+export default Test;
