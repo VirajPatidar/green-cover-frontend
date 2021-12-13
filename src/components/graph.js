96fca6a6c8702614af4a05706e3c8d175f6b2405
Viraj Patidar <vrj381@gmail.com>
2021-12-13 23:46:40 +0530

    graphs completed

---
+++ src/components/graph.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+import 'date-fns';
+import { format } from 'date-fns'
+import axios from 'axios';
+
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+import { Box, Button, Typography } from '@mui/material';
+import { teal } from '@mui/material/colors';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: teal[900],
+        },
+    },
+});
+
+
+export default function Graph() {
+    const [value, setValue] = useState(new Date("2012-01-01"));
+    const [value1, setValue1] = useState(new Date());
+    const [graphs, setGraphs] = useState(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Validation
+        let submit = true
+
+
+        if (value === "" || value1 === "") {
+            alert("Please select date")
+            submit = false
+        }
+
+        if (submit) {
+            setGraphs(null);
+            axios.post(`http://localhost:8000/vegetation`, {
+                "start_date": format(value, "yyyy-MM-dd"),
+                "end_date": format(value1, "yyyy-MM-dd"),
+            })
+                .then((res) => {
+                    console.log(res);
+                    setGraphs(res.data);
+                })
+                .catch(err => {
+                    console.log(err)
+                    console.log({ err })
+                    if (err.response.status === 400) {
+                        alert("an error occured. Please try again")
+                    }
+                });
+        }
+    };
+
+    useEffect(() => {  }, [graphs])
+
+
+    return (
+        <ThemeProvider theme={theme}>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                    label="Start Date"
+                    inputFormat="dd/MM/yyyy"
+                    value={value}
+                    onChange={(newValue) => {
+                        setValue(newValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                />
+                <span style={{ margin: 8 }}>{" "}</span>
+                <DatePicker
+                    label="End Date"
+                    inputFormat="dd/MM/yyyy"
+                    value={value1}
+                    onChange={(newValue) => {
+                        setValue1(newValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                />
+                <span style={{ margin: 8 }}>{" "}</span>
+                <Button type="submit" size="large" onClick={handleSubmit} variant="contained" color="primary" style={{ marginTop: "8px" }}>
+                    Get Graphs
+                </Button>
+            </LocalizationProvider>
+            {
+                graphs ?
+                    <Box sx={{ mt: 2 }}>
+                        <Typography variant="h6" gutterBottom sx={{ mt: 3, mb:0, pb:0 }}>
+                            NDVI-EVI vs Time Graph
+                        </Typography>
+                        <img src={graphs.indexes_graph} alt="indexes_graph" style={{ height: 400, width: 1300 }} />
+                        <Typography variant="h6" gutterBottom sx={{ mt: 3, mb:0, pb:0 }}>
+                            NDVI vs Time Graph
+                        </Typography>
+                        <img src={graphs.ndvi_graph} alt="ndvi_graph" style={{ height: 350, width: 1400 }} />
+                        <Typography variant="h6" gutterBottom sx={{ mt: 3, mb:0, pb:0 }}>
+                            EVI vs Time Graph
+                        </Typography>
+                        <img src={graphs.evi_graph} alt="evi_graph" style={{ height: 400, width: 1300 }} />
+                    </Box>
+                    :
+                    null
+            }
+        </ThemeProvider>
+    );
+}
--- src/components/vegetation.js
+++ src/components/vegetation.js
@@ -6,6 +6,7 @@ import MapFrame from './mapFrame';
 import { Typography, Box, Link, IconButton } from '@mui/material';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import InfoDialog from './infoDialog';
+import Graph from './graph';
 
 const Vegetation = () => {
 
@@ -33,7 +34,7 @@ const Vegetation = () => {
         <>
             {data
                 ?
-                <Box sx={{mb:5}}>
+                <Box sx={{ mb: 5 }}>
                     <Box sx={{ ml: 7, my: 4 }}>
                         <Typography variant="h5">
                             {data.name}
@@ -59,7 +60,23 @@ const Vegetation = () => {
                             <strong>EVI = </strong>2.5 * ((Band 5 - Band 4) / (Band 5 + 6 * Band 4 - 7.5 * Band 2 + 1))
                         </Typography>
                     </Box>
-                    <MapFrame url={data.map_url} height={"740"}/>
+                    <MapFrame url={data.map_url} height={"740"} />
+                    <Box sx={{ pt:2, ml: 7, my: 8 }}>
+                        <Typography variant="h5" gutterBottom component="div">
+                            Time series graphical analysis representing Vegetation Indexes (NDVI and EVI) of Maharashtra
+                        </Typography>
+                        <Typography variant="h7" gutterBottom component="div">
+                            Dataset Used:{" "}
+                            <Link href="https://developers.google.com/earth-engine/datasets/catalog/MODIS_006_MOD13Q1" target="_blank" rel="noopener">MOD13Q1.006 Terra Vegetation Indices 16-Day Global 250m</Link>
+                        </Typography>
+                        <Typography variant="body2" gutterBottom sx={{ mt: 2, maxWidth: "80%" }}>
+                            NDVI is the most commonly used remote sensing index that calculates the ratio of the difference and sum between the Near Infrared and Red bands of multispectral images. It normally takes values between -1 and +1. It is mostly used in vegetation dynamics monitoring, including biomass quantification. <br />
+                        </Typography>
+                        <Typography variant="body1" gutterBottom sx={{ mt:3, mb:2, maxWidth: "80%" }}>
+                            <strong>Enter start and end dates to get the EVI and NDVI graph for that duration:</strong> <br />
+                        </Typography>
+                        <Graph />
+                    </Box>
                     <InfoDialog
                         open={open}
                         closeDialog={close}
