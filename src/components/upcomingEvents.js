2f0db0aa5f5a47fccb7a7033d3209813fa14feed
Viraj Patidar <vrj381@gmail.com>
2022-01-05 16:49:39 +0530

    UpcomingEvents integrated with API

--- src/components/upcomingEvents.js
+++ src/components/upcomingEvents.js
@@ -1,4 +1,7 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+//MUI
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import MobileStepper from '@mui/material/MobileStepper';
@@ -12,38 +15,22 @@ import { autoPlay } from 'react-swipeable-views-utils';
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
-const data = [
-    {
-        id: 1,
-        label: 'Spread awareness: Use of a recycling bins',
-        place: 'Schools near NIT Nagpur, Nagpur',
-        date: '07/01/2022',
-        details: 'All nearby volunteers are requested to join us to spread awareness regarding importance of recycling.',
-    },
-    {
-        id: 2,
-        label: 'Spread awareness: Use of eco friendly bean bag fill',
-        place: 'Pune Institute of technology, Pune',
-        date: '08/01/2022',
-        details: 'All nearby volunteers are requested to join us to spread awareness regarding use of eco friendly bean bag fill.',
-    },
-    {
-        id: 3,
-        label: 'Plant trees',
-        place: 'Shivaji Park, Mumbai',
-        date: '09/01/2022',
-        details: 'All nearby volunteers are requested to join us to plant trees near shivaji park on sunday 9:00am. Saplings will be provided.',
-    },
-    {
-        id: 4,
-        label: 'Plant trees',
-        place: 'Nashik central Park, Nashik',
-        date: '10/01/2022',
-        details: 'All nearby volunteers are requested to join us to plant trees near Nashik central Park at 9:00am.',
-    },
-];
+const UpcomingEvents = () => {
 
-function UpcomingEvents() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/get-event`)
+            .then((res) => {
+                console.log(res);
+                console.log(res.data);
+                setData(res.data);
+            })
+            .catch(err => {
+                console.log(err)
+            });
+    }, [])
+
     const theme = useTheme();
     const [activeStep, setActiveStep] = React.useState(0);
     const maxSteps = data.length;
@@ -61,80 +48,90 @@ function UpcomingEvents() {
     };
 
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Typography variant="h5" gutterBottom component="div">
-                Upcoming Green Initiatives:
-            </Typography>
-            <Paper
-                square
-                elevation={0}
-                sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    height: 50,
-                    pl: 2,
-                    bgcolor: '#b2dfdb'
-                }}
-            >
-                <Typography variant="h6" component="div">
-                    {data[activeStep].label.toUpperCase()}
-                </Typography>
-            </Paper>
-            <AutoPlaySwipeableViews
-                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-                index={activeStep}
-                onChangeIndex={handleStepChange}
-                enableMouseEvents
-            >
-                {data.map((step, index) => (
-                    <div key={step.id}>
-                        {Math.abs(activeStep - index) <= 2 ? (
-                            <Box sx={{mx: 4, my: 4}}>
-                                <Typography variant="h6" gutterBottom component="div">
-                                    {data[activeStep].place}
-                                </Typography>
-                                <Typography variant="body1" gutterBottom component="div">
-                                    {data[activeStep].date}
-                                </Typography>
-                                <Typography variant="body1" gutterBottom component="div">
-                                    {data[activeStep].details}
-                                </Typography>
-                            </Box>
-                        ) : null}
-                    </div>
-                ))}
-            </AutoPlaySwipeableViews>
-            <MobileStepper
-                sx={{ backgroundColor: '#b2dfdb' }}
-                steps={maxSteps}
-                position="static"
-                activeStep={activeStep}
-                nextButton={
-                    <Button
-                        size="small"
-                        onClick={handleNext}
-                        disabled={activeStep === maxSteps - 1}
-                    >
-                        Next
-                        {theme.direction === 'rtl' ? (
-                            <KeyboardArrowLeft />
-                        ) : (
-                            <KeyboardArrowRight />
-                        )}
-                    </Button>
-                }
-                backButton={
-                    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-                        {theme.direction === 'rtl' ? (
-                            <KeyboardArrowRight />
-                        ) : (
-                            <KeyboardArrowLeft />
-                        )}
-                        Back
-                    </Button>
-                }
-            />
-        </Box>
+        <>
+            {
+                (data && data.length > 0) ?
+                    <Box sx={{ flexGrow: 1 }}>
+                        <Typography variant="h5" gutterBottom component="div">
+                            Upcoming Green Initiatives:
+                        </Typography>
+                        <Paper
+                            square
+                            elevation={0}
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                height: 50,
+                                pl: 2,
+                                bgcolor: '#b2dfdb'
+                            }}
+                        >
+                            <Typography variant="h6" component="div">
+                                <b>{data[activeStep].name}</b>
+                            </Typography>
+                        </Paper>
+                        <AutoPlaySwipeableViews
+                            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                            index={activeStep}
+                            onChangeIndex={handleStepChange}
+                            enableMouseEvents
+                        >
+                            {data.map((step, index) => (
+                                <div key={step.id}>
+                                    {Math.abs(activeStep - index) <= 2 ? (
+                                        <Box sx={{ mx: 4, my: 4 }}>
+                                            <Typography variant="h6" gutterBottom component="div">
+                                                <b>City: </b> {data[activeStep].city}
+                                            </Typography>
+                                            <Typography variant="h6" gutterBottom component="div" sx={{mt: 2}}>
+                                                <b>Venue: </b> {data[activeStep].address}
+                                            </Typography>
+                                            <Typography variant="h6" gutterBottom component="div" sx={{mt: 2}}>
+                                                <b>Date: </b> {data[activeStep].date}
+                                            </Typography>
+                                            <Typography variant="h6" gutterBottom component="div" sx={{mt: 2}}>
+                                                <b>Details: </b> <br /> {data[activeStep].details}
+                                            </Typography>
+                                        </Box>
+                                    ) : null}
+                                </div>
+                            ))}
+                        </AutoPlaySwipeableViews>
+                        <MobileStepper
+                            sx={{ backgroundColor: '#b2dfdb' }}
+                            steps={maxSteps}
+                            position="static"
+                            activeStep={activeStep}
+                            nextButton={
+                                <Button
+                                    size="small"
+                                    onClick={handleNext}
+                                    disabled={activeStep === maxSteps - 1}
+                                >
+                                    Next
+                                    {theme.direction === 'rtl' ? (
+                                        <KeyboardArrowLeft />
+                                    ) : (
+                                        <KeyboardArrowRight />
+                                    )}
+                                </Button>
+                            }
+                            backButton={
+                                <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                                    {theme.direction === 'rtl' ? (
+                                        <KeyboardArrowRight />
+                                    ) : (
+                                        <KeyboardArrowLeft />
+                                    )}
+                                    Back
+                                </Button>
+                            }
+                        />
+                    </Box>
+                    :
+                    null
+            }
+        </>
     );
 }
 
