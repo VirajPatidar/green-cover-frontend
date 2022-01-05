62aa147e6d0af0b38e5346563703186bf0ce3b40
Viraj Patidar <vrj381@gmail.com>
2022-01-05 16:03:26 +0530

    statistics received form backend API

--- src/components/statistics.js
+++ src/components/statistics.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 //MUI
 import { Box, Card, CardContent, Grid, Typography } from '@mui/material';
 
 const Statistics = () => {
+
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/statistics`)
+            .then((res) => {
+                console.log(res);
+                console.log(res.data);
+                setData(res.data);
+
+            })
+            .catch(err => {
+                console.log(err)
+            });
+    }, [])
+
     return (
-        <Box sx={{mb: 5, pb: 3}}>
+        <Box sx={{ mb: 5, pb: 3 }}>
             <Grid container spacing={6}>
                 <Grid item xs={12} sm={6} md={3}>
                     <Card raised sx={{ height: '100%', display: 'flex', flexDirection: 'column', backgroundColor: '#ffe082' }}>
@@ -14,7 +31,7 @@ const Statistics = () => {
                                 Active Volunteers
                             </Typography>
                             <Typography variant="h2">
-                                112
+                                {data && data.volunteers}
                             </Typography>
                         </CardContent>
                     </Card>
@@ -26,7 +43,7 @@ const Statistics = () => {
                                 Trees Planted
                             </Typography>
                             <Typography variant="h2">
-                                212
+                                {data && data.planted}
                             </Typography>
                         </CardContent>
                     </Card>
@@ -38,19 +55,19 @@ const Statistics = () => {
                                 Green Events
                             </Typography>
                             <Typography variant="h2">
-                                21
+                                {data && data.total_events}
                             </Typography>
                         </CardContent>
                     </Card>
                 </Grid>
                 <Grid item xs={12} sm={6} md={3}>
                     <Card raised sx={{ height: '100%', display: 'flex', flexDirection: 'column', backgroundColor: '#e6ee9c' }}>
-                        <CardContent sx={{ flexGrow: 1, textAlign: "center", pb: 0, mb: 0}}>
+                        <CardContent sx={{ flexGrow: 1, textAlign: "center", pb: 0, mb: 0 }}>
                             <Typography gutterBottom variant="h5" component="h2">
                                 Upcoming Events !
                             </Typography>
                             <Typography variant="h2">
-                                3
+                                {data && data.upcoming_events}
                             </Typography>
                         </CardContent>
                     </Card>
