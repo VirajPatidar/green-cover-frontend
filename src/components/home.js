758f60ef1207277c2da26925aab0a60608a8bb8c
Viraj Patidar <vrj381@gmail.com>
2022-02-05 17:34:42 +0530

    home page improved

--- src/components/home.js
+++ src/components/home.js
@@ -4,7 +4,7 @@ import React from 'react';
 import bannerlogo from '../images/bannerlogo.svg';
 
 //MUI
-import { Box, Button, Container, Link, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, Container, Link, Typography } from "@mui/material";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import HomeTable from './table';
 
@@ -26,8 +26,37 @@ const Home = () => {
                             Our interactive maps along with charts and related data enables students, researchers, environmentalists, etc; to get a fairly accurate estimate of green cover and its relation with other environmental factors.
                         </Typography>
                     </Box>
-                    <Box mt={4} fontSize="h6.fontSize" fontWeight={400} fontFamily="Monospace">
-                        This project is built by: <br /> <Link href="https://github.com/VirajPatidar" color="inherit" target="_blank" rel="noopener" underline="hover">Viraj Patidar</Link> & <Link href="https://github.com/atharvadpatil" color="inherit" target="_blank" rel="noopener" underline="hover">Atharva Patil</Link>
+                    <Box sx={{ display: "flex", justifyContent: "center" }} mt={4}>
+                        <Card sx={{ width: 375, backgroundColor: "#e0f2f1", borderRadius: "10px", borderColor: "#004d40" }} variant="outlined" >
+                            <CardContent>
+                                <Typography variant="h5" component="div">
+                                    Use our Analytics Tool
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    for <br /> environmentalists, scientists, researchers
+                                </Typography>
+                                <Typography variant="body2" sx={{ textAlign: "left" }}>
+                                    Use our analytics tool to observe green cover, land cover, air quality, forest loss, change in vegetation generate timelapses, graphs and other statistics.
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                        <Box sx={{ p: 2 }}></Box>
+                        <Card sx={{ width: 375, backgroundColor: "#e0f2f1", borderRadius: "10px", borderColor: "#004d40" }} variant="outlined" >
+                            <CardContent>
+                                <Typography variant="h5" component="div">
+                                    Become a Green Guardian
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    for <br /> students, volunteers, NGO's
+                                </Typography>
+                                <Typography variant="body2" sx={{ textAlign: "left" }}>
+                                    Register as a green guardian today and participate in our green events to make a difference and help reduce green cover depletion.
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Box>
+                    <Box mt={5} fontSize="h6.fontSize" fontWeight={400} fontFamily="Monospace">
+                        This web app is built by: <br /> <Link href="https://github.com/VirajPatidar" color="inherit" target="_blank" rel="noopener" underline="hover">Viraj Patidar</Link> & <Link href="https://github.com/atharvadpatil" color="inherit" target="_blank" rel="noopener" underline="hover">Atharva Patil</Link>
                     </Box>
                     <Box mt={3}>
                         <Link href="https://github.com/VirajPatidar/green-cover-backend" color="inherit" target="_blank" rel="noopener" underline="hover">
