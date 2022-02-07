ac62691046dc0e9d4e300041af827b86011710ac
Viraj Patidar <vrj381@gmail.com>
2022-02-07 11:33:51 +0530

    datagrid heatmap + table description

--- src/components/LC_Tables/ml_lc_district_class_table.js
+++ src/components/LC_Tables/ml_lc_district_class_table.js
@@ -1,29 +1,39 @@
 import * as React from 'react';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import { dbd_yby_mh_lc_class } from "../../data/dbd_yby_mh_lc_class";
+import { Box } from '@mui/material';
+import { teal } from '@mui/material/colors';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: teal[900],
+        },
+    },
+});
 
 const columns = [
-    { field: 'id', headerName: 'ID', width: 130 },
-    { field: 'year', headerName: 'Year', width: 130 },
-    { field: 'district', headerName: 'District', sortable: false, width: 130 },
-    { field: '1', headerName: 'Water', width: 130 },
-    { field: '2', headerName: 'Evergreen needleleaf forest', width: 130 },
-    { field: '3', headerName: 'Evergreen broadleaf forest', width: 130 },
-    { field: '4', headerName: 'Deciduous needleleaf forest', width: 130 },
-    { field: '5', headerName: 'Deciduous broadleaf forest', width: 130 },
-    { field: '6', headerName: 'Mixed forest', width: 130 },
-    { field: '7', headerName: 'Closed shrublands', width: 130 },
-    { field: '8', headerName: 'Open shrublands', width: 130 },
-    { field: '9', headerName: 'Woody savannas', width: 130 },
-    { field: '10', headerName: 'Savannas', width: 130 },
-    { field: '11', headerName: 'Grasslands', width: 130 },
-    { field: '12', headerName: 'Permanent wetlands', width: 130 },
-    { field: '13', headerName: 'Croplands', width: 130 },
-    { field: '14', headerName: 'Urban and built-up', width: 130 },
-    { field: '15', headerName: 'Cropland/natural vegetation mosaic', width: 130 },
-    { field: '16', headerName: 'Snow and ice', width: 130 },
-    { field: '17', headerName: 'Barren or sparsely vegetated', width: 130 },
+    { field: 'id', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'ID', width: 50 },
+    { field: 'year', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'Year', width: 130 },
+    { field: 'district', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'District', sortable: false, width: 130 },
+    { field: '1', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'Water', width: 130 },
+    { field: '2', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'Evergreen needleleaf forest', width: 150 },
+    // { field: '3', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'Evergreen broadleaf forest', width: 150 },
+    { field: '4', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'Deciduous needleleaf forest', width: 150 },
+    { field: '5', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'Deciduous broadleaf forest', width: 150 },
+    { field: '6', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'Mixed forest', width: 150 },
+    { field: '7', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'Closed shrublands', width: 150 },
+    { field: '8', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'Open shrublands', width: 150 },
+    { field: '9', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'Woody savannas', width: 150 },
+    { field: '10', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'Savannas', width: 150 },
+    { field: '11', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'Grasslands', width: 150 },
+    { field: '12', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'Permanent wetlands', width: 150 },
+    { field: '13', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'Croplands', width: 150 },
+    { field: '14', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'Urban and built-up', width: 150 },
+    // { field: '15', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'Cropland/natural vegetation mosaic', width: 150 },
+    { field: '16', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'Snow and ice', width: 150 },
+    { field: '17', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: "center", headerName: 'Barren or sparsely vegetated', width: 150 },
 ];
 
 const rows = dbd_yby_mh_lc_class.map((item, id) => Object.assign(item, { id }));
@@ -35,17 +45,70 @@ export default function MHLCClassDistrictTable() {
     const [pageSize, setPageSize] = React.useState(10);
 
     return (
-        <div style={{ height: 500, width: '100%' }}>
-            <DataGrid
-                rows={rows}
-                columns={columns}
-                pageSize={pageSize}
-                onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
-                rowsPerPageOptions={[10, 20, 30, 50, 100]}
-                pagination
-                checkboxSelection
-                components={{ Toolbar: GridToolbar }}
-            />
-        </div>
+        <ThemeProvider theme={theme}>
+            <Box sx={{
+                height: 550,
+                width: '100%',
+                '& .super-app-theme--header': {
+                    backgroundColor: '#004d40',
+                    color: '#ffffff',
+                    my: 2,
+                    py: 2
+                },
+                '& .cold1': {
+                    backgroundColor: '#e3f2fd',
+                },
+                '& .cold2': {
+                    backgroundColor: '#bbdefb',
+                },
+                '& .cold3': {
+                    backgroundColor: '#90caf9',
+                },
+                '& .hot1': {
+                    backgroundColor: '#ffccbc',
+                },
+                '& .hot2': {
+                    backgroundColor: '#ffab91',
+                },
+                '& .hot3': {
+                    backgroundColor: '#ff8a65',
+                },
+            }}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    getCellClassName={(params) => {
+                        if (params.field === 'year' || params.field === 'district' || params.field === 'id') {
+                            return '';
+                        }
+                        if (params.value < 15)
+                            return 'cold3'
+                        else if (params.value >= 15 && params.value < 50)
+                            return 'cold2'
+                        else if (params.value >= 50 && params.value < 100)
+                            return 'cold1'
+                        else if (params.value >= 100 && params.value < 500)
+                            return 'hot1'
+                        else if (params.value >= 500 && params.value < 1000)
+                            return 'hot2'
+                        else if (params.value >= 1000)
+                            return 'hot3'
+                    }}
+                    pageSize={pageSize}
+                    onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+                    rowsPerPageOptions={[10, 20, 30, 50, 100]}
+                    pagination
+                    components={{ Toolbar: GridToolbar }}
+                    sx={{
+                        boxShadow: 2,
+                        border: 2,
+                        borderColor: 'primary.light',
+                        '& .MuiDataGrid-cell:hover': {
+                            color: 'primary.main',
+                        },
+                    }}
+                />
+            </Box>
+        </ThemeProvider>
     );
 }
--- src/components/landCover.js
+++ src/components/landCover.js
@@ -54,7 +54,7 @@ const LandCover = () => {
                         </Typography>
                         <Typography variant="body2" gutterBottom sx={{ mt: 2, maxWidth: "80%" }}>
                             <strong sx={{ pb: 1 }}>Approach:</strong> <br />
-                            Aim is to predict landcover of Maharashtra for desired year using landcover data accessible via MODIS dataset. Since MODIS dataset is only available from 2001-01-01 to 2019-01-01, we can used supervised classification technique to predict landcover of Maharashtra after 2019-01-01.<br />
+                            Aim is to predict landcover of Maharashtra for desired year using landcover data accessible via MODIS dataset. Since MODIS dataset is only available from 2001-01-01 to 2020-01-01, we can used supervised classification technique to predict landcover of Maharashtra.<br />
                         </Typography>
                         <Typography variant="body2">
                             <ol>
@@ -73,29 +73,45 @@ const LandCover = () => {
                     </Box>
                     <MapFrame url={data.map_url} height={"660"} />
                     <Box mt={10}>
-                        <Typography variant="h5" gutterBottom  sx={{ ml: 7 }}>
+                        <Typography variant="h5" gutterBottom sx={{ ml: 7 }}>
                             Land cover classification of Maharashtra over the years (2001 to 2020)
+                        </Typography>
+                        <Typography variant="body2" gutterBottom sx={{ ml: 7, mt: 1, mb: 1, maxWidth: "80%" }}>
+                            <strong sx={{ pb: 1 }}>Dataset Used: </strong> <Link href="https://developers.google.com/earth-engine/datasets/catalog/MODIS_006_MCD12Q1" target="_blank" rel="noopener">MCD12Q1.006 MODIS Land Cover Type Yearly Global 500m</Link><br />
+                            The table below denotes land classification of Maharashtra from 2001 to 2020. The spread/area of each class can be seen against the desired year.<br />
                         </Typography>
                         <Box ml="auto" mr="auto" sx={{ maxWidth: 1410 }}>
                             <MHLCClassTable />
                         </Box>
-                        <Typography variant="h5" gutterBottom  sx={{ ml: 7, mt: 10 }}>
+                        <Typography variant="h5" gutterBottom sx={{ ml: 7, mt: 10 }}>
+                            District wise land cover classification of Maharashtra over the years (2001 to 2020)
+                        </Typography>
+                        <Typography variant="body2" gutterBottom sx={{ ml: 7, mt: 1, mb: 1, maxWidth: "80%" }}>
+                            <strong sx={{ pb: 1 }}>Dataset Used: </strong> <Link href="https://developers.google.com/earth-engine/datasets/catalog/MODIS_006_MCD12Q1" target="_blank" rel="noopener">MCD12Q1.006 MODIS Land Cover Type Yearly Global 500m</Link><br />
+                            The table below denotes district wise land classification of Maharashtra from 2001 to 2020. The spread/area of each class and each district can be seen against the desired year. <br /> The table is presented in the form of a heatmap to represent different range of values. <br /> Use the sorting, filtering and export feature for an enhanced experience.<br />
+                        </Typography>
+                        <Box ml="auto" mr="auto" sx={{ maxWidth: 1410 }}>
+                            <MHLCClassDistrictTable />
+                        </Box>
+                        <Typography variant="h5" gutterBottom sx={{ ml: 7, mt: 10 }}>
                             Change in land cover classification of Maharashtra over the years (2002 to 2020)
+                        </Typography>
+                        <Typography variant="body2" gutterBottom sx={{ ml: 7, mt: 1, mb: 1, maxWidth: "80%" }}>
+                            <strong sx={{ pb: 1 }}>Dataset Used: </strong> <Link href="https://developers.google.com/earth-engine/datasets/catalog/MODIS_006_MCD12Q1" target="_blank" rel="noopener">MCD12Q1.006 MODIS Land Cover Type Yearly Global 500m</Link><br />
+                            The table below denotes area change in land classification of Maharashtra from 2002 to 2020. The spread/area change of each class can be seen against the desired year. <br />Negative value denotes loss in cover and positive value denotes gain in cover.<br />
                         </Typography>
                         <Box ml="auto" mr="auto" sx={{ maxWidth: 1410 }}>
                             <MHLCClassChangeTable />
                         </Box>
-                        <Typography variant="h5" gutterBottom  sx={{ ml: 7, mt: 10 }}>
+                        <Typography variant="h5" gutterBottom sx={{ ml: 7, mt: 10 }}>
                             Percentage change in land cover classification of Maharashtra over the years (2002 to 2020)
+                        </Typography>
+                        <Typography variant="body2" gutterBottom sx={{ ml: 7, mt: 1, mb: 1, maxWidth: "80%" }}>
+                            <strong sx={{ pb: 1 }}>Dataset Used: </strong> <Link href="https://developers.google.com/earth-engine/datasets/catalog/MODIS_006_MCD12Q1" target="_blank" rel="noopener">MCD12Q1.006 MODIS Land Cover Type Yearly Global 500m</Link><br />
+                            The table below denotes percentage change in land classification of Maharashtra from 2002 to 2020. The percentage spread/area change of each class can be seen against the desired year. <br />  <strong>Formula: </strong> Change in area of class / Area of Maharashtra (307,713 km²) &times; 100<br />
                         </Typography>
                         <Box ml="auto" mr="auto" sx={{ maxWidth: 1410 }}>
                             <MHLCClassPercChangeTable />
-                        </Box>
-                        <Typography variant="h5" gutterBottom  sx={{ ml: 7, mt: 10 }}>
-                            District wise land cover classification of Maharashtra over the years (2001 to 2018)
-                        </Typography>
-                        <Box ml="auto" mr="auto" sx={{ maxWidth: 1410 }}>
-                            <MHLCClassDistrictTable />
                         </Box>
                     </Box>
                     <InfoDialog
